6932157e32f1254858fa38e0045e69c61ff96a03
AslanMakeG <Aslan2334@yandex.ru>
2023-12-05 09:50:35 +0300

    Перенести плагины

---
+++ scripts/plugin_API.js
@@ -0,0 +1,6 @@
+const add_button_code = '<p class="text-button" onclick="add_question_answer(this)">Добавить вариант ответа</p>';
+
+function load_plugins(){
+    //Перенести плагины на бекенд
+    //Вызов к апи
+}
