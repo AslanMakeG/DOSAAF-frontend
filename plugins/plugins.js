0f445cb54dfc4f10920d5c67573c1f7dcc0aaeca
AslanMakeG <Aslan2334@yandex.ru>
2023-12-09 21:12:05 +0300

    create test with plugins

---
+++ plugins/plugins.js
@@ -0,0 +1,193 @@
+class BaseQuestion{
+
+    //Создание верхушки вопроса (где вводится имя вопроса)
+    create_question_top(){
+        let top = document.createElement('div');
+        top.classList.add('question-top');
+        top.innerHTML = this.question_top_code;
+        return top;
+    }
+
+    //Создание самого вопроса (где добавляются варианты ответов)
+    create_question_body(code, addable_code){
+        let body = document.createElement('div');
+        body.classList.add('question-body');
+        body.innerHTML = code;
+        
+        //Если к вопросу можно добавить вариант ответа
+        if(addable_code){
+            let add_button = document.createElement('p');
+            add_button.classList.add('text-button');
+            add_button.textContent = 'Добавить вариант ответа';
+
+            add_button.addEventListener("click", function(e) {
+                let new_answer = document.createElement('div');
+                new_answer.classList.add('answer');
+                new_answer.innerHTML = addable_code;
+
+                this.parentNode.insertBefore(new_answer, this);
+            }, false);
+
+            body.appendChild(add_button);
+        }
+
+        return body;
+    }
+
+    //Отобразить вопрос (возвращает div элемент)
+    display_question(){
+        let question = document.createElement('div');
+        question.classList.add('question');
+        question.dataset.type = this.question_type;
+        
+        question.appendChild(this.question_top);
+        question.appendChild(this.question_body);
+
+        return question;
+    }
+
+    get_question_type(){
+        return this.question_type;
+    }
+
+    get_question_type_name(){
+        return this.question_type_name;
+    }
+
+    constructor(name, code, addable_code, question_type, question_type_name){
+        this.question_top_code = `<p class="question-top-title">Название вопроса:</p>
+                            <input class="question-top-input" type="text" name="question-name" placeholder="Введите название вопроса" value="${name}">`;
+        this.question_top = this.create_question_top();
+        this.question_body = this.create_question_body(code, addable_code);
+        this.question_type = question_type;
+        this.question_type_name = question_type_name;
+    }
+}
+
+class OneChoiceQuestion extends BaseQuestion{
+    constructor(question_name = '', name = ''){
+        super(
+            question_name,
+            `<div class='answer'><input type='radio' name="${name}" checked /><input class="answer-input" type='text' placeholder='Введите вариант ответа' /></div><div class='answer'><input type='radio' name="${name}" /><input class="answer-input" type='text' placeholder='Введите вариант ответа' /></div>`,
+            `<input type='radio' name="${name}" /><input class="answer-input" type='text' placeholder='Введите вариант ответа' />`,
+            'one',
+            'Один выбор'
+        );
+    }
+
+    static save_answers(question){
+        let answers = question.children[1].getElementsByClassName('answer');
+
+        let current_answer = 0;
+        let all_answers = [];
+        let right_answers = [];
+
+        Array.from(answers).forEach((answer) => {
+            let answer_name = answer.lastElementChild.value;
+
+            //Проверка на пустоту имени вопроса
+            if(!answer_name){
+                throw new Error('Заполните все варианты ответов');
+            }
+
+            all_answers.push(answer_name);
+
+            //Если этот вопрос оказался правильным, записываем в массив правильных ответов
+            if(answer.firstElementChild.checked){
+                right_answers.push(current_answer);
+            }
+
+            current_answer++;
+        });
+
+        return {'all_answers': all_answers, 'right_answers': right_answers}
+    }
+}
+
+class MultipleChoiceQuestion extends BaseQuestion{
+    constructor(question_name = '', name = ''){
+        super(
+            question_name,
+            `<div class='answer'><input type='checkbox' /><input class='answer-input' type='text' placeholder='Введите вариант ответа' /></div><div class='answer'><input type='checkbox' /><input class='answer-input' type='text' placeholder='Введите вариант ответа' /></div>`,
+            `<input type='checkbox' /><input class='answer-input' type='text' placeholder='Введите вариант ответа' />`,
+            'multiple',
+            'Множественный выбор'
+        );
+    }
+
+    static validate_answers(question){
+        //Проверяем, чтобы хотя бы один чекбокс был выделен 
+        let inputs = question.children[1].querySelectorAll('input[type=checkbox]');
+        let check = false; //Проверка, что хотя бы один чекбокс будет нажат
+        for(let i = 0; i < inputs.length; i++){
+            if(inputs[i].checked){
+                check = true;
+                break; //Выходим с цикла, если хотя бы один чекбокс выделен
+            }
+        }
+        //Если ни один чекбокс не выделен
+        if(!check){
+            throw new Error('В вопросах с множественным выбором выберите хотя бы один вариант ответа');
+        }
+    }
+
+    static save_answers(question){
+        this.validate_answers(question);
+        let answers = question.children[1].getElementsByClassName('answer');
+
+        let current_answer = 0;
+        let all_answers = [];
+        let right_answers = [];
+
+        Array.from(answers).forEach((answer) => {
+            let answer_name = answer.lastElementChild.value;
+
+            //Проверка на пустоту имени вопроса
+            if(!answer_name){
+                throw new Error('Заполните все варианты ответов');
+            }
+
+            all_answers.push(answer_name);
+
+            //Если этот вопрос оказался правильным, записываем в массив правильных ответов
+            if(answer.firstElementChild.checked){
+                right_answers.push(current_answer);
+            }
+
+            current_answer++;
+        });
+
+        return {'all_answers': all_answers, 'right_answers': right_answers}
+    }
+}
+
+class TextChoiceQuestion extends BaseQuestion{
+    constructor(question_name = '', name = ''){
+        super(
+            question_name,
+            `<div class='answer'><input class='answer-input' type='text' name="${name}" placeholder='Введите правильный ответ'/></div>`,
+            null,
+            'text',
+            'Текстовый ответ'
+        );
+    }
+
+    static save_answers(question){
+        let answers = question.children[1].getElementsByClassName('answer');
+
+        let right_answers = [];
+
+        let right_answer = answers[0].lastElementChild.value;
+
+        //Проверка на пустоту ответа
+        if(!right_answer){
+            throw new Error('Заполните все варианты ответов');
+        }
+
+        right_answers.push(right_answer.toLowerCase());
+
+        return {'all_answers': [], 'right_answers': right_answers}
+    }
+}
+
+const PLUGINS = [OneChoiceQuestion, MultipleChoiceQuestion, TextChoiceQuestion];
--- scripts/create_test.js
+++ scripts/create_test.js
@@ -6,80 +6,13 @@ function create_question(){
     let question_name = document.getElementById('question-name');
     let question_type = document.getElementById('question-type');
 
-    let question = new load_plugins[question_type.value](question_name.value, 'asdasd');
+    let question = new LOAD_PLUGINS[question_type.value](question_name.value, MD5(new Date()));
 
     questions.appendChild(question.display_question()); //Добавляем вопрос на страницу
 
     close_modal_window(); //Закрываем модальное окно и сбрасываем значения в нём
     question_name.value = '';
     question_type.value = question_type.firstElementChild.value;
-}
-
-//Добавить вариант ответа к вопросу
-function add_question_answer(type, button){
-    let last_answer = button.previousElementSibling;
-    let value = parseInt(last_answer.children[0].value) + 1;
-
-    let new_answer = document.createElement('div');
-    new_answer.classList.add('answer');
-
-    //Добавлять варианты ответа можно только к одному или множественному ответу
-    if(type === 'one'){
-        let name = last_answer.children[0].name;
-
-        new_answer.innerHTML = `<input type="radio" name="${name}" value="${value}" />
-        <input class="radio-question-text" type="text" placeholder="Введите вариант ответа" />`;
-    }
-
-    if(type === 'multiple'){
-        let name = last_answer.children[0].name.split('_')[0];
-
-        new_answer.innerHTML = `<input class="checkbox-question" type="checkbox" name="${name}_${value}" value="${value}" />
-        <input class="checkbox-question-text" type="text" placeholder="Введите вариант ответа" />`;
-    }
-
-    //Вставить новый вариант ответа перед кнопкой "Добавить вариант ответа"
-    button.parentNode.insertBefore(new_answer, button);
-}
-
-//Содержимое блока по типу вопроса
-function content_by_type(type){
-    //Если один ответ, то будет два варианта ответа с радиобатонами
-    if(type === 'one'){
-        return `<div class="question-body">
-            <div class="answer">
-                <input type="radio" name="answ${question_counter}" value="1" checked />
-                <input class="radio-question-text" type="text" placeholder="Введите вариант ответа" />
-            </div>
-            <div class="answer">
-                <input type="radio" name="answ${question_counter++}" value="2" />
-                <input class="radio-question-text" type="text" placeholder="Введите вариант ответа" />
-            </div>
-            <p class="text-button" onclick="add_question_answer('${type}', this)">Добавить вариант ответа</p>
-        </div>`;
-    }
-    //Если множественный ответ, то будет два варианта ответа с чекбоксами
-    if(type === 'multiple'){
-        return `<div class="question-body">
-            <div class="answer">
-                <input class="checkbox-question" type="checkbox" name="answ${question_counter}_1" value="1" />
-                <input class="checkbox-question-text" type="text" placeholder="Введите вариант ответа" />
-            </div>
-            <div class="answer">
-                <input class="checkbox-question" type="checkbox" name="answ${question_counter++}_2" value="2" />
-                <input class="checkbox-question-text" type="text" placeholder="Введите вариант ответа" />
-            </div>
-            <p class="text-button" onclick="add_question_answer('${type}', this)">Добавить вариант ответа</p>
-        </div>`;
-    }
-    //Если ответ текстом, то будет поле для ввода верного ответа
-    if(type === 'text'){
-        return `<div class="question-body">
-            <div class="answer">
-                <input class="text-question-text" type="text" name="answ${question_counter++}" placeholder="Введите правильный ответ"/>
-            </div>
-        </div>`;
-    }
 }
 
 //Вывод ошибки при создании теста
@@ -89,7 +22,6 @@ function raise_creation_error(error){
     return;
 }
 
-
 //Создать тест (json)
 function create_test(){
     try{
@@ -97,7 +29,7 @@ function create_test(){
         
         //Проверка на пустоту имени теста
         if(!test_name){
-            throw new Error('Заполните название вопроса');
+            throw new Error('Заполните название теста');
         }
 
         let test_json = {
@@ -127,56 +59,14 @@ function create_test(){
                 throw new Error('Заполните все названия вопросов');
             }
 
-            let answers = question.children[1].getElementsByClassName('answer');
-            let current_answer = 0; //Номер текущего ответа в вопросе, чтобы записать правильные ответы
+            let saved_answers = LOAD_PLUGINS[question_type].save_answers(question);
 
-            //Если множественный выбор, то проверяем, чтобы хотя бы один чекбокс был выделен 
-            if(question_type === 'multiple'){
-                let inputs = question.children[1].querySelectorAll('input[type=checkbox]');
-                let check = false; //Проверка, что хотя бы один чекбокс будет нажат
-
-                for(let i = 0; i < inputs.length; i++){
-                    if(inputs[i].checked){
-                        check = true;
-                        break; //Выходим с цикла, если хотя бы один чекбокс выделен
-                    }
-                }
-
-                //Если ни один чекбокс не выделен
-                if(!check){
-                    throw new Error('В вопросах с множественным выбором выберите хотя бы один вариант ответа');
-                }
-            }
-
-            Array.from(answers).forEach((answer) => {
-                let answer_name = answer.lastElementChild.value;
-                
-                //Проверка на пустоту имени вопроса
-                if(!answer_name){
-                    throw new Error('Заполните все варианты ответов');
-                }
-
-                //Если один или множественный выбор, то записываем ответы в массив ответов
-                if(question_type === 'one' || question_type === 'multiple'){
-                    question_json['answers'].push(answer_name);
-                    
-                    //Если этот вопрос оказался правильным, записываем в массив правильных ответов
-                    if(answer.firstElementChild.checked){
-                        question_json['right_answer'].push(current_answer);
-                    }
-                }
-
-                //Если ответ текстом, то записываем его сразу в массив правильных ответов
-                if(question_type === 'text'){
-                    question_json['right_answer'].push(answer_name.toLowerCase());
-                }
-
-                current_answer++;
-            });
+            question_json['answers'] = saved_answers['all_answers'];
+            question_json['right_answer'] = saved_answers['right_answers'];
 
             test_json['questions'].push(question_json); //Записываем вопрос в массив вопросов
         });
-
+        
         post_test_on_server(test_json);
     }
     catch(e){
